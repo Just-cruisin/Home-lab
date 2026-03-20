9b2aa202b52e7fd71ffd6b992d3f3cf88a418cb0
Just-cruisin <Thomas.halling1999@gmail.com>
2026-03-21 00:57:13 +1100

    Update compress_directory

--- scripts/compress_directory.sh
+++ scripts/compress_directory.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 # This script is used to compress a directory and save it to a backup location 
+
+directory="$1"
+
+if [ -z "$directory" ]
+then
+	echo "Usage: ./compress_directory.sh <directory>"
+	exit 1
+fi
+
+if [ ! -d "$directory" ]
+then
+	echo "Error: $directory does not exist or is not a directory"
+	exit 1
+fi
+
+backup_name="$(basename $directory)_$(date +%Y-%m-%d)"
+
+tar -czf /home/tom/Documents/backups/$backup_name.tar.gz $directory
+
+find /home/tom/Documents/backups -mtime +7 -delete
+
+echo "Directory: $directory compressed!"
