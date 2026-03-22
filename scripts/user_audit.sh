64ad11c93efb73e7ca59d83da4b2fbc4fc07cb21
Just-cruisin <Thomas.halling1999@gmail.com>
2026-03-23 00:30:03 +1100

    Updated user_audit

--- scripts/user_audit.sh
+++ scripts/user_audit.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 user_list=$(cat /etc/passwd | grep "bin/bash" | awk -F ':' '{print $1}')
 
+echo "Last logins for each user"
+echo "-------------------------"
+
+
+
 for user in $user_list
 do
-	echo $user
-	echo "Last logged in: $(last -R $user | head -n 1 | awk '{print $3, $4, $5, $6}')"
+	last_login=$(last -R $user | head -n 1)
+	if [ -z "$last_login" ]
+	then
+		echo "$user has never logged in"
+	else
+		echo $user
+		echo "Last logged in: $(last -R $user | head -n 1 | awk '{print $3, $4, $5, $6}')"
+		
+	fi
 done
