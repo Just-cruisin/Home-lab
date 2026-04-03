dd680f2842ff15d77af133ad02f2c9fac4eadbca
Thomas Halling <thomas.halling1999@gmail.com>
2026-04-03 14:47:43 +0200

    update dashboard.py

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 from disk_usage import get_disk_usage
 from service_status import get_service_status
 from network_monitor import get_network_status
-from prom_scapper import get_node_metric
+from prom_scrapper import get_node_metrics
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 print(os.path.join(base_dir, '..', 'flask_templates'))
@@ -14,8 +14,8 @@ def index():
     service_statuses = get_service_status(["ssh","cron","tailscaled"])
     disk_percentage,status = get_disk_usage()
     network_statuses = get_network_status()
-    item, metric = get_node_metric
-    return render_template('index.html', disk_percentage=disk_percentage, status=status,service_statuses=service_statuses,network_statuses=network_statuses,metric=metric)
+    node_metrics = get_node_metrics
+    return render_template('index.html', disk_percentage=disk_percentage, status=status,service_statuses=service_statuses,network_statuses=network_statuses,node_metrics=node_metrics)
 
 
 if __name__ == '__main__':
